2886863fdc338c6eb11cb6dd933cbbba368f3709
tooeztech_lory_jiang <funtree9@icloud.com>
2019-11-29 11:09:18 +0800

    remove database config

--- config/config.js
+++ config/config.js
@@ -6,16 +6,5 @@ module.exports = {
     key: 'xxx',
     expiresIn: '1d'
   },
-  database: {
-    username: 'root',
-    password: null,
-    database: '{{projectInfo.name}}',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    define: {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    }
-  },
   preference: {}
 }
